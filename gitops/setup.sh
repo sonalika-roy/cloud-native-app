5b75d56f10b82ecda842ca9da39623622cd986a4
Auto <sonalikaroy@microsoft.com>
2021-10-21 00:30:06 +0000

    Auto commit prep files

--- gitops/setup.sh
+++ gitops/setup.sh
@@ -2,20 +2,20 @@
 
 curl -s https://fluxcd.io/install.sh | sudo bash
 
-wget https://github.com/smallstep/cli/releases/download/v0.15.2/step-cli_0.15.2_amd64.deb
+sudo wget https://github.com/smallstep/cli/releases/download/v0.15.2/step-cli_0.15.2_amd64.deb
 sudo dpkg -i step-cli_0.15.2_amd64.deb
 
-cd cloud-native-app/gitops/infrastructure/linkerd
+cd /cloud-native-app/gitops/infrastructure/linkerd
 
-step certificate create identity.linkerd.cluster.local ca.crt ca.key \
+sudo step certificate create identity.linkerd.cluster.local ca.crt ca.key \
 	--profile root-ca --no-password --insecure \
 	--san identity.linkerd.cluster.local
 
-step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
+sudo step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
 	--ca ca.crt --ca-key ca.key --profile intermediate-ca --not-after 8760h --no-password --insecure \
 	--san identity.linkerd.cluster.local
 
-kubectl -n linkerd create secret generic certs \
+sudo kubectl -n linkerd create secret generic certs \
 	--from-file=ca.crt --from-file=issuer.crt \
 	--from-file=issuer.key -oyaml --dry-run=client \
 	> certs.yaml
@@ -28,38 +28,38 @@ registryHostDnsLabel=`echo $registryHost | cut -d '.' -f 1`
 
 
 exp=$(date -d '+8760 hour' +"%Y-%m-%dT%H:%M:%SZ")
-sed -i "s/{cert_expiry}/$exp/g" gitops/clusters/production/infrastructure-linkerd.yaml
-sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/infrastructure-harbor.yaml
-sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-harbor.yaml
-sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-seed.yaml
-sed -i "s/{cluster_issuer_email}/$cluster_issuer_email/g" gitops/clusters/production/infrastructure-certmanager.yaml
+sudo sed -i "s/{cert_expiry}/$exp/g" /cloud-native-app/gitops/clusters/production/infrastructure-linkerd.yaml
+sudo sed -i "s/{registryHost}/$registryHost/g" /cloud-native-app/gitops/clusters/production/infrastructure-harbor.yaml
+sudo sed -i "s%{registryUrl}%$registryUrl%g" /cloud-native-app/gitops/clusters/production/infrastructure-harbor.yaml
+sudo sed -i "s%{registryUrl}%$registryUrl%g" /cloud-native-app/gitops/clusters/production/infrastructure-seed.yaml
+sudo sed -i "s/{cluster_issuer_email}/$cluster_issuer_email/g" /cloud-native-app/gitops/clusters/production/infrastructure-certmanager.yaml
 
-sed -i "s/{cicdWebhookHost}/$appHostName/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{appHostName}/$appHostName/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{sendGridApiKey}/$sendGridApiKey/g" gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{cicdWebhookHost}/$appHostName/g" /cloud-native-app/gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{registryHost}/$registryHost/g" /cloud-native-app/gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{appHostName}/$appHostName/g" /cloud-native-app/gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{sendGridApiKey}/$sendGridApiKey/g" /cloud-native-app/gitops/clusters/production/app-devops.yaml
 
-sed -i "s/{registryHostDnsLabel}/$registryHostDnsLabel/g" gitops/clusters/production/infrastructure-harbor-nginx.yaml
-sed -i "s/{appHostDnsLabel}/$appHostDnsLabel/g" gitops/clusters/production/infrastructure-nginx.yaml
+sudo sed -i "s/{registryHostDnsLabel}/$registryHostDnsLabel/g" /cloud-native-app/gitops/clusters/production/infrastructure-harbor-nginx.yaml
+sudo sed -i "s/{appHostDnsLabel}/$appHostDnsLabel/g" /cloud-native-app/gitops/clusters/production/infrastructure-nginx.yaml
 
 cd gitops/app/core
 
-kubectl create secret docker-registry regcred \
+sudo kubectl create secret docker-registry regcred \
 	--docker-server="https://$registryHost" --docker-username=conexp  --docker-password=FTA@CNCF0n@zure3  --docker-email=user@mycompany.com -n conexp-mvp -oyaml --dry-run=client \
 	> regcred-conexp.yaml
 
-kubectl create secret docker-registry regcred \
+sudo kubectl create secret docker-registry regcred \
 	--docker-server="https://$registryHost" --docker-username=conexp  --docker-password=FTA@CNCF0n@zure3  --docker-email=user@mycompany.com -n openfaas-fn -oyaml --dry-run=client \
 	> regcred-openfaas.yaml
 
 cd ../../..
 
-git remote set-url origin "https://$owner:$GITHUB_TOKEN@github.com/$owner/cloud-native-app.git"
-git config user.email "$cluster_issuer_email"
-git config user.name "Auto"
-git add *
-git commit -m  "Auto commit prep files"
-git push -u origin main
+sudo git remote set-url origin "https://$owner:$GITHUB_TOKEN@github.com/$owner/cloud-native-app.git"
+sudo git config user.email "$cluster_issuer_email"
+sudo git config user.name "Auto"
+sudo git add *
+sudo git commit -m  "Auto commit prep files"
+sudo git push -u origin main
 
 flux bootstrap github \
 	  --owner="$owner" \
